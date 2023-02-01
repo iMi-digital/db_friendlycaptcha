7419ac831ee6b614774516c17f719871df9347bf
Lukas Hormel <l.hormel@imi.de>
2023-02-01 12:03:45 +0100

    Move User Model To Appropriate Directory

--- Configuration/Extbase/Persistence/Classes.php
+++ Configuration/Extbase/Persistence/Classes.php
@@ -3,10 +3,10 @@
 return [
     \In2code\Femanager\Domain\Model\User::class => [
         'subclasses' => [
-            \BalatD\FriendlyCaptcha\Domain\Model\Domain\User::class
-        ]
+            '\BalatD\FriendlyCaptcha\Domain\Model\User' => \BalatD\FriendlyCaptcha\Domain\Model\User::class,
+        ],
     ],
-    \BalatD\FriendlyCaptcha\Domain\Model\Domain\User::class => [
+    \BalatD\FriendlyCaptcha\Domain\Model\User::class => [
         'tableName' => 'fe_users',
         'recordType' => 0,
     ],
